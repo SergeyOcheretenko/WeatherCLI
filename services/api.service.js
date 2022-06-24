47e0a95090e845c0d7b017955133043e3fb8990a
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-24 14:26:16 +0300

    Change 'https' for 'axios'

--- services/api.service.js
+++ services/api.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import https from 'https';
+import axios from 'axios';
 import { getKeyValue } from './storage.service.js';
 
 async function getWeather(city) {
@@ -8,22 +8,15 @@ async function getWeather(city) {
     if (!token) {
         throw new Error('API key is not set');
     }
-    const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
-    url.searchParams.append('q', city);
-    url.searchParams.append('appid', token);
-    url.searchParams.append('lang', 'ua');
-    url.searchParams.append('units', 'metric');
-    
-    https.get(url, (response) => {
-        let res = '';
-        response.on('data', (chunk) => {
-            res += chunk;
-        });
-
-        response.on('end', () => {
-            console.log(res);
-        });
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'ua',
+            units: 'metric'
+        }
     });
+    return data;
 }
 
 export { getWeather };
