8d7cdd17c61ada7e962165b5ed34ee6ff24b3849
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-24 09:35:16 +0300

    Add validate cmd arguments

--- weather.js
+++ weather.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
+'use strict';
 
-const initCLI = () => {
-    console.log('Started');
+import { getArgs } from "./helpers/args.js";
+
+function initCLI() {
+    const args = getArgs(process.argv);
+    console.log(args);
+    if (args.h) {
+        // help
+    }
+    if (args.s) {
+        // save city
+    }
+    if (args.t) {
+        // token
+    }
+    // show weather
 };
 
 initCLI();
